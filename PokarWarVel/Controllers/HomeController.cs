1befc81e93f66b335b9a4b4a4c94e7a7d5a8d341
WadCharene <h.charenegmail.com>
2016-11-21 16:03:45 +0100

    ...

---
+++ PokarWarVel/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MarvelApi;
+using MarvelApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PokarWarVel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MarvelRequester r = new MarvelRequester();
+            List<Characters> info = r.GetCharacters(limit: 100);
+
+            return View(info);
+          
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PokarWarVel/Controllers/searchController.cs
@@ -0,0 +1,49 @@
+﻿using MarvelApi;
+using MarvelApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PokarWarVel.Controllers
+{
+    public class searchController : Controller
+    {
+        // GET: search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Results(string search)
+        {
+
+            MarvelRequester r = new MarvelRequester();
+            List<Characters> LAliste = r.GetCharacters(limit: 100);
+            //filtre
+
+            //List<Characters> trouve = new List<Characters>();
+
+            //foreach (Characters Current in LAliste)
+            //{
+            //     if (Current.name==search)
+            //     {
+
+            //         trouve.Add(Current);
+
+            //     }
+            //}
+
+
+            //return View(trouve);
+
+            //LINQ
+
+            return View(LAliste.Where(l => l.name.Contains(search)));
+
+
+        }
+    }
+}
